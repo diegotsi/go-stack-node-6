f4ea6019c6bdfb67a7ee5a0ca8f387b0310ecf8d
diego <diegooliveira.tsi@gmail.com>
2020-05-05 10:29:28 -0300

    feat: import data from csv

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,61 @@
+import { getCustomRepository, getRepository, In } from 'typeorm';
+import csvParse from 'csv-parse';
+import fs from 'fs';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import TransactionService from './CreateTransactionService';
 
+interface Request {
+  fileName: string;
+}
+
+interface CSVTransaction {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute({ fileName }: Request): Promise<Transaction[]> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+    const categoriesRepository = getRepository(Category);
+    const transactionService = new TransactionService();
+
+    const contactsReadStream = fs.createReadStream(fileName);
+
+    const parsers = csvParse({
+      from_line: 2,
+    });
+
+    const parseCSV = contactsReadStream.pipe(parsers);
+
+    const transactions: CSVTransaction[] = [];
+    const categories: string[] = [];
+
+    parseCSV.on('data', async line => {
+      const [title, type, value, category] = line.map((cell: string) =>
+        cell.trim(),
+      );
+
+      if (!title || !type || !value) return;
+
+      categories.push(category);
+
+      transactions.push({ title, type, value, category });
+    });
+
+    await new Promise(resolve => parseCSV.on('end', resolve));
+
+    const createdTransactions: Transaction[] = [];
+
+    await transactions.map(async transaction => {
+      const t = await transactionService.execute(transaction);
+
+      createdTransactions.push(t);
+    });
+
+    return createdTransactions;
   }
 }
 
